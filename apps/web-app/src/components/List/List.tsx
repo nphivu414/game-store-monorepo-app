80c7fd7a499a6958bc05b58d7da1e4744bba6ef9
Vu Nguyen <nphivu414@gmail.com>
2021-08-02 22:03:13 +0700

    - Update list component

--- apps/web-app/src/components/List/List.tsx
+++ apps/web-app/src/components/List/List.tsx
@@ -10,3 +10,35 @@ type ListItem = {
   subTitle: string;
 }
 
+const List: React.FC<ListProps> = ({
+  data
+}) => {
+  if (!data) {
+    return null;
+  }
+
+  return (
+    <div>
+      {
+        data.map((item) => {
+          const { title, subTitle, avatarUrl } = item;
+          return (
+            <div className="grid grid-cols-5">
+              <div className="flex justify-center items-center">
+                <div className="avatar">
+                  <div className="mb-8 rounded-full w-10 h-10">
+                    <img src={avatarUrl} alt=""/>
+                  </div>
+                </div>
+              </div>
+              <div className="flex col-span-4">
+                
+              </div>
+            </div>
+          )
+        })
+      }
+    </div>
+  )
+}
+
