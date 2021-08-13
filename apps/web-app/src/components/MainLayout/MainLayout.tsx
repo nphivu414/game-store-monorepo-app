5353b1e62a261d1b12d7afcba555cde846e40a8b
Vu Nguyen <nphivu414@gmail.com>
2021-08-13 19:08:12 +0700

    - Update mainlayout max width

--- apps/web-app/src/components/MainLayout/MainLayout.tsx
+++ apps/web-app/src/components/MainLayout/MainLayout.tsx
@@ -6,7 +6,7 @@ import BottomTab from '../BottomTab';
 const MainLayout: React.FC = ({ children }) => {
   const { theme } = React.useContext(ThemeContext);
   return (
-    <div className="bg-base-300 max-w-sm text-base-content m-auto" data-theme={theme}>
+    <div className="bg-base-300 md:max-w-sm text-base-content m-auto" data-theme={theme}>
       <NavigationBar isSticky />
       <div className="min-h-screen">{children}</div>
       <BottomTab />
