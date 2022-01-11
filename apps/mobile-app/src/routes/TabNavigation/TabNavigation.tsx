0d2a5cc5dfd52d32b48b04365317ecde5b0b01eb
Vu Nguyen <nphivu414@gmail.com>
2022-01-11 15:58:56 +0700

    - Add react-native-vector-icons

--- apps/mobile-app/src/routes/TabNavigation/TabNavigation.tsx
+++ apps/mobile-app/src/routes/TabNavigation/TabNavigation.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-// import Icon from 'react-native-vector-icons/FontAwesome5';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import HomeStack from '../Stacks/HomeStack';
 
 type TabNavigationProps = {};
@@ -12,37 +12,36 @@ type TabBarIconProps = {
 const Tab = createBottomTabNavigator();
 const { Navigator } = Tab;
 
-// const renderTabBarIcon = (routeName: string) => {
-//   return (props: TabBarIconProps) => {
-//     const {color} = props;
-//     let iconName = 'home';
-//     switch (routeName) {
-//       case 'Explore':
-//         iconName = 'compass';
-//         break;
-//       case 'Activity':
-//         iconName = 'history';
-//         break;
-//       case 'Notifications':
-//         iconName = 'bell';
-//         break;
-//       case 'Account':
-//         iconName = 'user';
-//         break;
-//       case 'Documentation':
-//         iconName = 'book';
-//         break;
-//       default:
-//         break;
-//     }
-//     return <Icon name={iconName} solid size={24} color={color} />;
-//   };
-// };
+const renderTabBarIcon = (routeName: string) => {
+  return (props: TabBarIconProps) => {
+    const { color } = props;
+    let iconName = 'home';
+    switch (routeName) {
+      case 'Explore':
+        iconName = 'explore';
+        break;
+      default:
+        break;
+    }
+    return <Icon name={iconName} solid size={24} color={color} />;
+  };
+};
 
 const TabNavigation: React.FC<TabNavigationProps> = () => {
   return (
-    <Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="HomeTab" component={HomeStack} />
+    <Navigator
+      initialRouteName="Home"
+      screenOptions={(props) => {
+        const {
+          route: { name: routeName },
+        } = props;
+        return {
+          headerShown: false,
+          tabBarIcon: renderTabBarIcon(routeName),
+        };
+      }}
+    >
+      <Tab.Screen name="Explore" component={HomeStack} />
     </Navigator>
   );
 };
